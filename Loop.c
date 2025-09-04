0f325a4b17ea6968fb04da9975447194ebd77d14
gajrajpatel <gl570646@gmail.com>
2025-09-04 20:48:45 +0530

    prime no

--- Loop.c
+++ Loop.c
@@ -9,7 +9,10 @@ int main(){
    }
    printf("%d\n",sum);
 }*/
-int  num=123;
+
+
+
+/*int  num=123;
 int sum=0;
 while(num>0)
 {
@@ -17,6 +20,39 @@ while(num>0)
     sum =sum+ lastdigit;
     num =num/10;
 }
-printf("sum of digit = %d",num);
+printf("sum of digit = %d\n",sum);
 return 0;
+}*/
+
+/*for(int i =0; i<=5; i++){
+    if (i==3){
+        break;
+    }
+    printf("%d",i);
 }
+}*/
+
+/*int a=11;
+int isprime=1;
+if("n<=1"){
+    printf("prime no hai ");
+}
+else{
+    printf("prime no nhi hai ");
+    return 0;
+}
+}*/
+
+/*int n=5;
+int t1=0;
+int t2=1;
+int next;
+for(int i=1; i<=n;i++){
+    printf("%d\n",t1);
+    next=t1+t2;
+    t1=t2;
+    t2=next;
+}
+return 0;
+}*/
+
